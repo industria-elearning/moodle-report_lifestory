620637e8ab66ce1a3b91103621b73a86740af0d7
PieroLS04 <piero@datacurso.com>
2025-11-08 20:02:43 -0500

    feat: translations

---
+++ lang/es_mx/report_lifestory.php
@@ -0,0 +1,61 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     report_lifestory
+ * @category    string
+ * @copyright   2025 Datacurso
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['activity'] = 'Actividad';
+$string['altlogo'] = 'Logo Datacurso';
+$string['clearselection'] = 'Limpiar';
+$string['course'] = 'Curso';
+$string['coursetotal'] = 'Total del curso';
+$string['error_ai_service'] = 'Error del servicio de IA: {$a}';
+$string['error_airequest'] = 'Error al comunicarse con el servicio de IA: {$a}';
+$string['exportcsv'] = 'Exportar a CSV';
+$string['exportingcsv'] = 'Exportando csv';
+$string['feedback'] = 'Retroalimentacion';
+$string['feedbackfromai'] = 'Feedback de IA';
+$string['generatefeedback'] = 'Genera feedback con IA';
+$string['generatingfeedback'] = 'Generando feedback';
+$string['gradepercent'] = 'Nota (%)';
+$string['lifestory'] = 'Historia de vida del estudiante';
+$string['lifestory:generateaifeedback'] = 'Generar retroalimentación con IA para los estudiantes';
+$string['lifestory:view'] = 'Vea el informe de la historia de vida';
+$string['noreportdata'] = 'No hay datos de informe disponibles.';
+$string['noresponse'] = 'No se recibió respuesta.';
+$string['pluginname'] = 'Historia de vida del estudiante IA';
+$string['privacy:metadata:ai_provider'] = 'Se envían datos al sistema de IA de la empresa para generar retroalimentación basada en el historial académico del estudiante.';
+$string['privacy:metadata:ai_provider:context'] = 'El contexto del análisis (por ejemplo, desempeño académico o reflexión).';
+$string['privacy:metadata:ai_provider:courseids'] = 'La lista de IDs de cursos en los que el usuario está matriculado.';
+$string['privacy:metadata:ai_provider:coursenames'] = 'Los nombres de los cursos en los que el usuario está matriculado.';
+$string['privacy:metadata:ai_provider:fullname'] = 'El nombre completo del usuario para proporcionar contexto.';
+$string['privacy:metadata:ai_provider:userid'] = 'El ID del usuario cuyo historial académico se analiza.';
+$string['range'] = 'Rango';
+$string['report_lifestory:generateaifeedback'] = 'Generar retroalimentación con IA sobre el desempeño del estudiante';
+$string['searchusers'] = 'Buscar usuarios';
+$string['section'] = 'Seccion';
+$string['select'] = 'Seleccionar';
+$string['selectuser'] = 'Por favor seleccione un usuario para ver su historia de vida';
+$string['total'] = 'Total';
+$string['unexpected_ai_error'] = 'Error inesperado en el procesamiento de IA: {$a}';
